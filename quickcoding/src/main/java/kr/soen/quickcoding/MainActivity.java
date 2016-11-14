901f02ef0a6a39151de4f7d464c569cafc8082d8
koko0757022 <koko0757022@naver.com>
2016-11-14 10:43:06 +0900

    test

--- quickcoding/src/main/java/kr/soen/quickcoding/MainActivity.java
+++ quickcoding/src/main/java/kr/soen/quickcoding/MainActivity.java
@@ -1,27 +1,21 @@
 package kr.soen.quickcoding;
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
 import java.util.LinkedList;
 
-
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final LinkedList<String> s = new LinkedList<>();
-        final LinkedList<Integer> i = new LinkedList<>();
+        final LinkedList<String> s = new LinkedList<String>();
+        final LinkedList<Integer> i = new LinkedList<Integer>();
         Button btn = (Button)findViewById(R.id.button);
         Button btn2 = (Button)findViewById(R.id.button2);
         final EditText edit = (EditText)findViewById(R.id.edit);
@@ -34,22 +28,27 @@ public class MainActivity extends AppCompatActivity {
 
                 String value =  edit.getText().toString();
 
+
                 try {
-                    int value2 =  Integer.parseInt(value);
+                    Integer value2 = Integer.valueOf(value);
+
                     i.add(value2);
+
                 }catch(NumberFormatException e){
                     s.add(value);
+
                 }
             }
         });
+
         btn2.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View v) {
-                for(Integer num : i){
-                    t1.setText(num);
+                for(Integer num1 : i){
+                    t1.setText(""+num1);
                 }
-                for(String num : s){
-                    t2.setText(num);
+                for(String num2 : s){
+                    t2.setText(num2);
                 }
             }
         });
